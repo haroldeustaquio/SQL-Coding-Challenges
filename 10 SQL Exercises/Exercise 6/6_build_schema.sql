eb8e4849368c54def5a2fa674ce44282a5d17d6c
harold1735 <harold.eustaquio.a@uni.pe>
2024-09-24 15:57:54 -0600

    Exercise 6 ... completed

--- 10 SQL Exercises/Exercise 6/6_build_schema.sql
+++ 10 SQL Exercises/Exercise 6/6_build_schema.sql
@@ -33,24 +33,24 @@ INSERT INTO Scientists(SSN,Name)
     (845657245,'Elizabeth Doe'),
     (845657246,'Kumar Swamy');
 
- INSERT INTO Projects ( Code,Name,Hours)
- VALUES ('AeH1','Winds: Studying Bernoullis Principle', 156),
-       ('AeH2','Aerodynamics and Bridge Design',189),
-       ('AeH3','Aerodynamics and Gas Mileage', 256),
-       ('AeH4','Aerodynamics and Ice Hockey', 789),
-       ('AeH5','Aerodynamics of a Football', 98),
-       ('AeH6','Aerodynamics of Air Hockey',89),
-       ('Ast1','A Matter of Time',112),
-       ('Ast2','A Puzzling Parallax', 299),
-       ('Ast3','Build Your Own Telescope', 6546),
-       ('Bte1','Juicy: Extracting Apple Juice with Pectinase', 321),
-       ('Bte2','A Magnetic Primer Designer', 9684),
-       ('Bte3','Bacterial Transformation Efficiency', 321),
-       ('Che1','A Silver-Cleaning Battery', 545),
-       ('Che2','A Soluble Separation Solution', 778);
+INSERT INTO Projects ( Code,Name,Hours)
+VALUES ('AeH1','Winds: Studying Bernoullis Principle', 156),
+      ('AeH2','Aerodynamics and Bridge Design',189),
+      ('AeH3','Aerodynamics and Gas Mileage', 256),
+      ('AeH4','Aerodynamics and Ice Hockey', 789),
+      ('AeH5','Aerodynamics of a Football', 98),
+      ('AeH6','Aerodynamics of Air Hockey',89),
+      ('Ast1','A Matter of Time',112),
+      ('Ast2','A Puzzling Parallax', 299),
+      ('Ast3','Build Your Own Telescope', 6546),
+      ('Bte1','Juicy: Extracting Apple Juice with Pectinase', 321),
+      ('Bte2','A Magnetic Primer Designer', 9684),
+      ('Bte3','Bacterial Transformation Efficiency', 321),
+      ('Che1','A Silver-Cleaning Battery', 545),
+      ('Che2','A Soluble Separation Solution', 778);
 
- INSERT INTO AssignedTo ( Scientist, Project)
-   VALUES (123234877,'AeH1'),
+INSERT INTO AssignedTo ( Scientist, Project)
+  VALUES (123234877,'AeH1'),
     (152934485,'AeH3'),
     (222364883,'Ast3'),	   
     (326587417,'Ast3'),
--- 10 SQL Exercises/Exercise 6/6_questions.sql
+++ 10 SQL Exercises/Exercise 6/6_questions.sql
@@ -1,5 +1,19 @@
--- https://en.wikibooks.org/wiki/SQL_Exercises/Scientists
 -- 6.1 List all the scientists' names, their projects' names, 
     -- and the hours worked by that scientist on each project, 
     -- in alphabetical order of project name, then scientist name.
+
+	select c.name as ProjectName, a.name, c.hours from scientists as a
+	inner join AssignedTo as b
+	on b.scientist = a.SSN
+	inner join Projects as c
+	on b.project = c.code
+	order by ProjectName asc, a.name asc
+
 -- 6.2 Select the project names which are not assigned yet
+
+	select a.name from projects as a
+	left join assignedto as b
+	on a.code = b.project
+	where b.scientist is null
+
+
