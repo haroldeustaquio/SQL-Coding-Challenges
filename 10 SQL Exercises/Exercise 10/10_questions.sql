8c0036730931b598e86a7f5843fa8ca61b2e1111
harold1735 <harold.eustaquio.a@uni.pe>
2024-09-24 19:50:40 -0600

    Exercise 10 - completed ... 100%

--- 10 SQL Exercises/Exercise 10/10_questions.sql
+++ 10 SQL Exercises/Exercise 10/10_questions.sql
@@ -1,4 +1,22 @@
 -- 10.1 Join table PEOPLE and ADDRESS, but keep only one address information for each person (we don't mind which record we take for each person). 
     -- i.e., the joined table should have the same number of rows as table PEOPLE
+
+	select b.id,b.name,c.address,c.updatedate from people as b 
+	inner join
+	(select * from
+	(select id,address,updatedate, ROW_NUMBER() over(partition by id order by address) as alias
+	from address) as a
+	where a.alias=1) as  c
+	on b.id = c.id
+
+
 -- 10.2 Join table PEOPLE and ADDRESS, but ONLY keep the LATEST address information for each person. 
     -- i.e., the joined table should have the same number of rows as table PEOPLE
+
+	select b.id,b.name,c.address,c.updatedate from people as b 
+	inner join
+	(select * from
+	(select id,address,updatedate, ROW_NUMBER() over(partition by id order by address desc) as alias
+	from address) as a
+	where a.alias=1) as  c
+	on b.id = c.id
